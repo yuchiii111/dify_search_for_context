60041f3f7092a8bcf234630bee2d70dea10574b1
yuchiii111 <153926062+yuchiii111@users.noreply.github.com>
2024-01-31 17:25:10 +0800

    Update keyword_search.py

--- keyword_search.py
+++ keyword_search.py
@@ -1,17 +1,12 @@
 from Jieba_Keyword_Table_Handler import JiebaKeywordTableHandler
 from langchain.schema import BaseRetriever, Document
 from collections import defaultdict
-from extensions.ext_database import db ?
-from models.dataset import Dataset, DatasetKeywordTable, DocumentSegment ?
-from core.index.base import BaseIndex ?
 
-1.BaseIndex
-2.db
-3.documentsegment
+
 
 class KeywordTableIndex():
    
-    def create(self, texts: list[Document], **kwargs) -> BaseIndex:
+    def create_keyword_table(self, texts: list[Document], **kwargs):
         keyword_table_handler = JiebaKeywordTableHandler()
         keyword_table = {}
         # keyword_table = {key = keyword, value = set(doc.id)}
@@ -20,24 +15,8 @@ class KeywordTableIndex():
             keywords = keyword_table_handler.extract_keywords(text.page_content)
             # self._update_segment_keywords(text.metadata['doc_id'], list(keywords))
             keyword_table = self._add_text_to_keyword_table(keyword_table, text.metadata['doc_id'], list(keywords))
-
-        dataset_keyword_table = DatasetKeywordTable(
-            dataset_id=self.dataset.id,
-            keyword_table=json.dumps({
-                '__type__': 'keyword_table',
-                '__data__': {
-                    "index_id": self.dataset.id,
-                    "summary": None,
-                    "table": {}
-                }
-            }, cls=SetEncoder)
-        )
-        db.session.add(dataset_keyword_table)
-        db.session.commit()
-
-        self._save_dataset_keyword_table(keyword_table)
-
-        return self
+           
+        return keyword_table
 
     def _add_text_to_keyword_table(self, keyword_table: dict, id: str, keywords: list[str]) -> dict:
         for keyword in keywords:
@@ -46,32 +25,26 @@ class KeywordTableIndex():
             keyword_table[keyword].add(id)
         return keyword_table
 
+    def _retrieve_ids_by_query(self, keyword_table: dict, query: str, k: int = 4):
+        keyword_table_handler = JiebaKeywordTableHandler()
+        # 提取查询中的关键词，并返回set(str)
+        keywords = keyword_table_handler.extract_keywords(query)
 
+        # go through text chunks in order of most matching keywords
+        chunk_indices_count: Dict[str, int] = defaultdict(int)
+        keywords = [keyword for keyword in keywords if keyword in set(keyword_table.keys())]
+        for keyword in keywords:
+            for node_id in keyword_table[keyword]:
+                chunk_indices_count[node_id] += 1
 
-def _retrieve_ids_by_query(self, keyword_table: dict, query: str, k: int = 4):
-    keyword_table_handler = JiebaKeywordTableHandler()
-    # 提取查询中的关键词，并返回set(str)
-    keywords = keyword_table_handler.extract_keywords(query)
+        sorted_chunk_indices = sorted(
+            list(chunk_indices_count.keys()),
+            key=lambda x: chunk_indices_count[x],
+            reverse=True,
+        )
 
-    # go through text chunks in order of most matching keywords
-    chunk_indices_count: Dict[str, int] = defaultdict(int)
-    keywords = [keyword for keyword in keywords if keyword in set(keyword_table.keys())]
-    for keyword in keywords:
-        for node_id in keyword_table[keyword]:
-            chunk_indices_count[node_id] += 1
-
-    sorted_chunk_indices = sorted(
-        list(chunk_indices_count.keys()),
-        key=lambda x: chunk_indices_count[x],
-        reverse=True,
-    )
-
-    return sorted_chunk_indices[: k]
-
-
-
-
-
+        return sorted_chunk_indices[: k]
+        # return[docid1,docid2,docid3,docid4]
 
 
 
