073bef5b9162d01814ca8b66b8d342f4ba769d5c
yuchiii111 <153926062+yuchiii111@users.noreply.github.com>
2024-01-31 16:34:38 +0800

    Update keyword_search.py

--- keyword_search.py
+++ keyword_search.py
@@ -1,5 +1,56 @@
+from Jieba_Keyword_Table_Handler import JiebaKeywordTableHandler
+from langchain.schema import BaseRetriever, Document
+from collections import defaultdict
+from extensions.ext_database import db ?
+from models.dataset import Dataset, DatasetKeywordTable, DocumentSegment ?
+from core.index.base import BaseIndex ?
+
+1.BaseIndex
+2.db
+3.documentsegment
+
+class KeywordTableIndex():
+   
+    def create(self, texts: list[Document], **kwargs) -> BaseIndex:
+        keyword_table_handler = JiebaKeywordTableHandler()
+        keyword_table = {}
+        # keyword_table = {key = keyword, value = set(doc.id)}
+        
+        for text in texts:
+            keywords = keyword_table_handler.extract_keywords(text.page_content)
+            # self._update_segment_keywords(text.metadata['doc_id'], list(keywords))
+            keyword_table = self._add_text_to_keyword_table(keyword_table, text.metadata['doc_id'], list(keywords))
+
+        dataset_keyword_table = DatasetKeywordTable(
+            dataset_id=self.dataset.id,
+            keyword_table=json.dumps({
+                '__type__': 'keyword_table',
+                '__data__': {
+                    "index_id": self.dataset.id,
+                    "summary": None,
+                    "table": {}
+                }
+            }, cls=SetEncoder)
+        )
+        db.session.add(dataset_keyword_table)
+        db.session.commit()
+
+        self._save_dataset_keyword_table(keyword_table)
+
+        return self
+
+    def _add_text_to_keyword_table(self, keyword_table: dict, id: str, keywords: list[str]) -> dict:
+        for keyword in keywords:
+            if keyword not in keyword_table:
+                keyword_table[keyword] = set()
+            keyword_table[keyword].add(id)
+        return keyword_table
+
+
+
 def _retrieve_ids_by_query(self, keyword_table: dict, query: str, k: int = 4):
     keyword_table_handler = JiebaKeywordTableHandler()
+    # 提取查询中的关键词，并返回set(str)
     keywords = keyword_table_handler.extract_keywords(query)
 
     # go through text chunks in order of most matching keywords
@@ -16,3 +67,12 @@ def _retrieve_ids_by_query(self, keyword_table: dict, query: str, k: int = 4):
     )
 
     return sorted_chunk_indices[: k]
+
+
+
+
+
+
+
+
+
