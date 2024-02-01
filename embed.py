561eff2205747762f124a1016b37c7418a432e34
yuchiii111 <153926062+yuchiii111@users.noreply.github.com>
2024-02-01 11:25:54 +0800

    Update embed.py

--- embed.py
+++ embed.py
@@ -1,35 +1,34 @@
 from langchain.embeddings import HuggingFaceBgeEmbeddings
 from langchain.vectorstores import Chroma
 import numpy as np
+from typing import List
 
-def embedding(documents):
-    text_embeddings = []
-    model_name = "BAAI/bge-Large-zh"
-    model_kwargs = {'device': 'cpu'}
-    encode_kwargs = {'normalize_embeddings': True}
-    embeddings = HuggingFaceBgeEmbeddings(
-                    model_name=model_name,
-                    model_kwargs=model_kwargs,
-                    encode_kwargs=encode_kwargs,
-                    query_instruction="为文本生成向量表示用于文本检索"
-                )
+class embed():
+    def embedding_query(self, text) -> List[float]:
+        model_name = "BAAI/bge-Large-zh"
+        model_kwargs = {'device': 'cpu'}
+        encode_kwargs = {'normalize_embeddings': True}
+        model = HuggingFaceBgeEmbeddings(
+                        model_name=model_name,
+                        model_kwargs=model_kwargs,
+                        encode_kwargs=encode_kwargs,
+                        query_instruction="为文本生成向量表示用于文本检索"
+                    )
+        embedding_result = model.ivoke_text_embedding(texts=text)
+        text_embeddings = self.norm_embedding(embedding_result.embeddings)
+        return text_embeddings
 
-    for vector in embeddings:
-        normalized_embedding = (vector / np.linalg.norm(vector)).tolist()
-        text_embeddings.append(normalized_embedding)
+    def norm_embedding(self, embeddings) -> List[float]:
+        text_embeddings = []
+         for vector in embeddings:
+            normalized_embedding = (vector / np.linalg.norm(vector)).tolist()
+            text_embeddings.append(normalized_embedding)
+         return text_embeddings
 
-    return text_embeddings
-
-            for i in range(0, len(texts), max_chunks):
-                batch_texts = texts[i:i + max_chunks]
-
-                embedding_result = self._model_instance.invoke_text_embedding(
-                    texts=batch_texts,
-                    user=self._user
-                )
-
-                for vector in embedding_result.embeddings:
-                    try:
-                        normalized_embedding = (vector / np.linalg.norm(vector)).tolist()
-                        text_embeddings.append(normalized_embedding)
+    def embedding_documents(self, documents) -> List[List[float]]:
+        embedding_result_ = []
+        for i in range(0, len(texts)):
+            batch_texts = documents[i:i + 1]      
+            embedding_result_.append(self.embedding_query(batch_texts)) 
+        return embedding_result_
             
