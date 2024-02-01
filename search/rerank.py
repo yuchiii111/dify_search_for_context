d2dec0588029bd2f3dd17349a5e687ba581afd5d
yuchiii111 <153926062+yuchiii111@users.noreply.github.com>
2024-02-01 17:00:02 +0800

    Update rerank.py

--- search/rerank.py
+++ search/rerank.py
@@ -2,7 +2,7 @@ from typing import List, Optional
 from langchain.schema import Document
 import cohere
 
-api=?
+# api=? 其他写完了
 
 class RerankRunner:
 
@@ -12,13 +12,13 @@ class RerankRunner:
               
         docs = []
         doc_id = []
-        unique_documents = []
+        
         for document in documents:
             if document.metadata['doc_id'] not in doc_id:
                 # 汇总所有需要排列的doc并去重
                 doc_id.append(document.metadata['doc_id'])
                 docs.append(document.page_content)
-                unique_documents.append(document)
+                
 
         api_key = ''
         co = cohere.Client(api_key)
@@ -28,14 +28,13 @@ class RerankRunner:
             model=model,
             top_n=top_n
         )
-        rerank_result
               
         "/home/yuhp/llm_models/all_models/rank_model/bge-reranker-base"
 
-        rerank_documents = []
+        rerank_results = []
         for idx, result in enumerate(results):
             # format document
-            rerank_document = RerankDocument(
+            rerank_result = RerankDocument(
                 index=result.index,
                 text=result.document['text'],
                 score=result.relevance_score,
@@ -44,23 +43,14 @@ class RerankRunner:
             # score threshold check
             if score_threshold is not None:
                 if result.relevance_score >= score_threshold:
-                    rerank_documents.append(rerank_document)
+                    rerank_results.append(rerank_result)
             else:
-                rerank_documents.append(rerank_document)
+                rerank_results.append(rerank_result)
 
-        return RerankResult(
-            model=model,
-            docs=rerank_documents
-        )
-
-
-
-
-
-        # 规范结果输出而已，后面先不看
+        # 规范结果输出
         rerank_documents = []
 
-        for result in rerank_result.docs:
+        for result in rerank_results:
             # format document
             rerank_document = Document(
                 page_content=result.text,
@@ -68,10 +58,16 @@ class RerankRunner:
                     "doc_id": documents[result.index].metadata['doc_id'],
                     "doc_hash": documents[result.index].metadata['doc_hash'],
                     "document_id": documents[result.index].metadata['document_id'],
-                    "dataset_id": documents[result.index].metadata['dataset_id'],
                     'score': result.score
                 }
             )
             rerank_documents.append(rerank_document)
 
-        return rerank_documents -> list[doc, , ]
+        return rerank_documents 
+        # -> list[doc, , ]
+                
+class RerankDocument:
+    index: int
+    text: str
+    score: float
+    
