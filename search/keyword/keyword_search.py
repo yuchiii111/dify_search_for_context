7c4ca98c0f1e902814ec8247306b9f14d29ce586
yuchiii111 <153926062+yuchiii111@users.noreply.github.com>
2024-02-05 15:26:43 +0800

    Update keyword_search.py

--- search/keyword/keyword_search.py
+++ search/keyword/keyword_search.py
@@ -1,17 +1,20 @@
 from search.keyword.Jieba_Keyword_Table_Handler import JiebaKeywordTableHandler
 from langchain.schema import BaseRetriever, Document
 from collections import defaultdict
+from typing import Dict,Set,List
 
 
 
 class KeywordTableIndex():
+    def __init__ (self, texts: List[Document]):
+       self._texts = texts
    
-    def create_keyword_table(self, texts: list[Document], **kwargs):
+    def create_keyword_table(self, **kwargs) ->Dict(str: Set(str)) :
         keyword_table_handler = JiebaKeywordTableHandler()
         keyword_table = {}
         # keyword_table = {key = keyword, value = set(doc.id)}
         
-        for text in texts:
+        for text in self._texts:
             keywords = keyword_table_handler.extract_keywords(text.page_content)
             # self._update_segment_keywords(text.metadata['doc_id'], list(keywords))
             keyword_table = self._add_text_to_keyword_table(keyword_table, text.metadata['doc_id'], list(keywords))
@@ -42,9 +45,12 @@ class KeywordTableIndex():
             key=lambda x: chunk_indices_count[x],
             reverse=True,
         )
+       
+        result_id = sorted_chunk_indices[: k]
+        docs = [doc for doc in self._texts if doc.metadata['doc_id'] in result_id]
 
-        return sorted_chunk_indices[: k]
-        # return[docid1,docid2,docid3,docid4]
+        return docs
+        # return[doc]
 
 
 
