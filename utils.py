27d1981e5b54f8349fd19a0c14c8bc50d1fa5316
yuchiii111 <153926062+yuchiii111@users.noreply.github.com>
2024-02-06 17:24:09 +0800

    Update utils.py

--- utils.py
+++ utils.py
@@ -6,46 +6,28 @@ import transformers.data.metrics.squad_metrics as squad_metrics
 def doc_to_text(doc):
     # Given a passage p, the conversation history {q1, a1, . . . qi−1, ai−1}
     # and a question qi, the task is to predict the answer ai
-    doc_text = doc["context"] + "\n\n"
-    for q, a in zip_longest(
-        doc["questions"]["input_text"], doc["answers"]["input_text"][:-1]
-    ):  # omit target answer ai
-        question = f"Q: {q}\n\n"
-        answer = f"A: {a}\n\n" if a is not None else "A:"
-        doc_text += question + answer
+    preprompt = ""
+    doc_text = preprompt + "\n\n" + "context:{{"
+    for a in doc["context"]: 
+        doc_text += doc["context"] + "\n\n"
+    q = doc["FAQ问题"]
+    question = f"Q: {q}\n\n"
+    answer = f"A:"
+    doc_text += "}}" + question + answer
     return doc_text
 
 
 def doc_to_target(doc):
-    turn_id = len(doc["questions"]["input_text"])
-    # Returns unique answers and valid alternatives (Some questions in CoQA have multiple valid answers).
-    answers = []
-    answer_forturn = doc["answers"]["input_text"][turn_id - 1]
-    answers.append(answer_forturn)
-
-    additional_answers = doc.get("additional_answers")
-    if additional_answers:
-        for key in additional_answers:
-            additional_answer_for_turn = additional_answers[key]["input_text"][
-                turn_id - 1
-            ]
-            if additional_answer_for_turn.lower() not in map(str.lower, answers):
-                answers.append(additional_answer_for_turn)
+    answers = "A:" + doc["FAQ回答"]
     return answers
 
 
 def em(gold_list, pred):
     # tests for exact match and on the normalised answer (compute_exact)
     em_sum = 0.0
-    if len(gold_list) > 1:
-        for i in range(len(gold_list)):
-            gold_answers = gold_list[0:i] + gold_list[i + 1 :]
-            # predictions compared against (n) golds and take maximum
-            em_sum += max(squad_metrics.compute_exact(a, pred) for a in gold_answers)
-    else:
-        em_sum += max(squad_metrics.compute_exact(a, pred) for a in gold_list)
+    em_sum += max(squad_metrics.compute_exact(gold_list, pred))
 
-    return em_sum / max(1, len(gold_list))
+    return em_sum
 
 
 def compute_scores(gold_list, pred):
@@ -53,19 +35,13 @@ def compute_scores(gold_list, pred):
     # test for overlap (compute_f1)
     f1_sum = 0.0
     em_sum = 0.0
-    if len(gold_list) > 1:
-        for i in range(len(gold_list)):
-            gold_answers = gold_list[0:i] + gold_list[i + 1 :]
-            # predictions compared against (n) golds and take maximum
-            em_sum += max(squad_metrics.compute_exact(a, pred) for a in gold_answers)
-            f1_sum += max(squad_metrics.compute_f1(a, pred) for a in gold_answers)
-    else:
-        em_sum += max(squad_metrics.compute_exact(a, pred) for a in gold_list)
-        f1_sum += max(squad_metrics.compute_f1(a, pred) for a in gold_list)
+    
+    em_sum += max(squad_metrics.compute_exact(gold_list, pred))
+    f1_sum += max(squad_metrics.compute_f1(gold_list, pred))
 
     return {
-        "em": em_sum / max(1, len(gold_list)),
-        "f1": f1_sum / max(1, len(gold_list)),
+        "em": em_sum,
+        "f1": f1_sum),
     }
 
 
@@ -75,6 +51,4 @@ def process_results(doc, results):
 
     scores = compute_scores(gold_list, pred)
     return scores
-~
-~
-~
+    
