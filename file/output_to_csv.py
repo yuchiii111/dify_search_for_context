7593c66d36048ac9931b56b86f82aa0b022694ef
yuchiii111 <153926062+yuchiii111@users.noreply.github.com>
2024-02-06 13:21:36 +0800

    Update output_to_csv.py

--- file/output_to_csv.py
+++ file/output_to_csv.py
@@ -2,15 +2,41 @@ from search.hy_search import search
 from search.vector.embed import embed
 from search.keyword.keyword_search import KeywordTableIndex
 import pandas as pd
+from langchain.document_loaders import TextLoader,CSVLoader
+from langchain.document_loaders import PyPDFLoader
+from langchain.text_splitter import CharacterTextSplitter
+
 
 loader1 = CSVLoader(file_path='')
-loader2 = (file_path='')
-loader3 = (file_path='')
-loader4 = (file_path='')
+loader2 = CSVLoader(file_path='')
+loader3 = PyPDFLoader(file_path='')
+loader4 = TextLoader(file_path='')
+query_csv_file_path = " "
+
 doc1 = loader1.load()
 doc2 = loader2.load()
 doc3 = loader3.load()
 doc4 = loader4.load()
+
+# 处理pdf
+for pdf_id in range(1,len(doc3))
+    doc3[0].page_content = doc3[0].page_content+doc3[pdf_id].page_content
+    doc3.pop(pdf_id)
+
+def load_split(documents):
+    text_splitter = CharacterTextSplitter(separator="/n/n",chunk_overlap=0)
+    documents = text_splitter.split_documents(documents)
+    return documents
+
+base_doc = []
+for doc_sp in [doc1,doc2,doc3,doc4]:
+    sp_doc = load_split(doc_sp).
+    base_doc.extend(sp_doc)
+
+for doc_id, doc_seg in enumerate(base_doc):
+        doc_seg["metadata"].clear()
+        doc_seg["metadata"]["doc_id"] = doc_id + 1    
+
 
 vec_embed = embed()
 doc_embed = [doc.page_content for doc in base_doc]
@@ -19,8 +45,7 @@ embeddings = vec_embed.embedding_documents(doc_embed)
 keyword_search = KeywordTableIndex(base_doc)
 keywtab = keyword_search.create_keyword_table()
 
-csc_file_path = " "
-df = pd.read_csv(csv_file_path, encoding='utf-8')
+df = pd.read_csv(query_csv_file_path, encoding='utf-8')
 query_list = df.['FAQ问题'].tolist()
 
 run_search = search()
@@ -30,6 +55,6 @@ for query in query_list：
     final_context.append([doc.page_content for doc in final_doc])
 
 df['context'] = final_context
-df.to_csv(csv_file_pahth, index=False, encoding='utf-8')
+df.to_csv(query_csv_file_path, index=False, encoding='utf-8')
     
 
