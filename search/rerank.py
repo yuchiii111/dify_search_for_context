7a3ee3c5c26c7debe5821ca3b66b5fdf5c5d6a98
yuchiii111 <153926062+yuchiii111@users.noreply.github.com>
2024-02-18 15:59:15 +0800

    Update rerank.py

--- search/rerank.py
+++ search/rerank.py
@@ -1,6 +1,6 @@
 from typing import List, Optional
 from langchain.schema import Document
-import cohere
+# import cohere
 import torch
 from transformers import AutoModelForSequenceClassification, AutoTokenizer
 
